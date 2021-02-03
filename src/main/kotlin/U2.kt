59e051fd98aab8bead2c4907198e8f1a095a0dad
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-03 12:38:33 -0500

    Add `U1` and `U2` subclasses from `Rocket`.
    Override launch and land methods with custom chance of failure.

---
+++ src/main/kotlin/U2.kt
@@ -0,0 +1,21 @@
+package org.quebin31.spacechallenge
+
+import kotlin.random.Random
+
+class U2 : Rocket() {
+    override val cargoLimit: Int = 11 * 1000 // kg
+
+    override fun launch(): Boolean {
+        val chanceOfExplosion = 0.04 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return random < chanceOfExplosion
+    }
+
+    override fun land(): Boolean {
+        val chanceOfCrash = 0.08 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return random < chanceOfCrash
+    }
+}
