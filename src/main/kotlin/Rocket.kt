b42822a6e43bc4dd1266f5c6b96f594dd9ae29e3
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-03 17:03:28 -0500

    Moved subclasses into Rocket.kt.

--- src/main/kotlin/Rocket.kt
+++ src/main/kotlin/Rocket.kt
@@ -1,5 +1,7 @@
 package org.quebin31.spacechallenge
 
+import kotlin.random.Random
+
 abstract class Rocket : SpaceShip {
     abstract val cost: Int
     abstract val cargoLimit: Int
@@ -13,5 +15,43 @@ abstract class Rocket : SpaceShip {
         if (!canCarry(item)) return
 
         cargoCarried += item.weight
+    }
+}
+
+class U1 : Rocket() {
+    override val cost = 100 // millions US$
+    override val cargoLimit: Int = 8 * 1000 // kg
+
+    override fun launch(): Boolean {
+        val chanceOfExplosion = 0.05 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return !(random < chanceOfExplosion)
+    }
+
+    override fun land(): Boolean {
+        val chanceOfCrash = 0.01 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return !(random < chanceOfCrash)
+    }
+}
+
+class U2 : Rocket() {
+    override val cost = 120 // millions US$
+    override val cargoLimit: Int = 11 * 1000 // kg
+
+    override fun launch(): Boolean {
+        val chanceOfExplosion = 0.04 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return !(random < chanceOfExplosion)
+    }
+
+    override fun land(): Boolean {
+        val chanceOfCrash = 0.08 * (cargoCarried / cargoLimit)
+        val random = Random.nextDouble()
+
+        return !(random < chanceOfCrash)
     }
 }
