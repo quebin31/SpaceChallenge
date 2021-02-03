f893ee2bc55a3821ec1363ce2ab7cdb1c71e4f1b
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-03 13:35:24 -0500

    Added methods to load U1 and U2 rockets.

--- src/main/kotlin/Simulation.kt
+++ src/main/kotlin/Simulation.kt
@@ -18,4 +18,27 @@ class Simulation {
         return ArrayList(items)
     }
 
+    private fun <R : Rocket> loadRockets(items: Iterable<Item>, factory: () -> R): ArrayList<R> {
+        val rockets = arrayListOf<R>()
+        var currentRocket = factory()
+
+        for (item in items) {
+            if (!currentRocket.canCarry(item)) {
+                rockets.add(currentRocket)
+                currentRocket = factory()
+            } else {
+                currentRocket.carry(item)
+            }
+        }
+
+        return ArrayList(rockets)        
+    }
+
+    fun loadU1(items: Iterable<Item>): ArrayList<U1> {
+        return loadRockets(items) { U1() }
+    }
+
+    fun loadU2(items: Iterable<Item>): ArrayList<U2> {
+        return loadRockets(items) { U2() }
+    }
 }
