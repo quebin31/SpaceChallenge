319b831a830f3b7234b7d1f0998d838b2296ba5a
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-07 14:09:55 -0500

    Change main function to meet exact requirements.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,16 +7,21 @@ fun main(args: Array<String>) {
         return
     }
 
-    for (phase in args) {
-        println("Simulation for phase: $phase")
-        val items = Simulation.loadItems(phase)
+    val phaseItems = args.map { phase -> Simulation.loadItems(phase) }
 
+    // Simulation with u1 rockets
+    println("Simulation with U1 rockets")
+    for ((index, items) in phaseItems.withIndex()) {
         val u1Rockets = Simulation.loadU1(items)
         val u1Budget = Simulation.runSimulation(u1Rockets)
-        println(":: Needed budget with U1 rockets: \$$u1Budget millions")
+        println(":: Needed budget for phase ${index + 1}: \$$u1Budget millions")
+    }
 
+    // Simulation with u2 rockets
+    println("Simulation with U2 rockets")
+    for ((index, items) in phaseItems.withIndex()) {
         val u2Rockets = Simulation.loadU2(items)
         val u2Budget = Simulation.runSimulation(u2Rockets)
-        println(":: Needed budget with U2 rockets: \$$u2Budget millions")
+        println(":: Needed budget for phase ${index + 1}: \$$u2Budget millions")
     }
 }
